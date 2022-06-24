488e06037ef209be69663e78c78cba5eb02ac27f
StevenWeir038 <weir_steven@btinternet.com>
2022-06-24 16:26:53 +0000

    Integrate pagination in all_products view

--- products/views.py
+++ products/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from .models import Product
 
+# Pagination
+from django.core.paginator import Paginator
+
 
 def all_products(request):
     """ A view to show all products including sort by and search queries """
-    
-    products = Product.objects.all()
+
+    # Set up pagination
+    p = Paginator(Product.objects.all(), 10)
+    page = request.GET.get('page')
+    products = p.get_page(page)
 
     context = {
         'products': products,
