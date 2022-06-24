faad18fe4bfbb2538fbd3e2d38375c5fefba6990
StevenWeir038 <weir_steven@btinternet.com>
2022-06-24 13:17:51 +0000

    Configure basic product admin view

--- products/admin.py
+++ products/admin.py
@@ -2,5 +2,27 @@ from django.contrib import admin
 from .models import Category, Product
 
 
-admin.site.register(Category)
-admin.site.register(Product)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+    ordering = ('friendly_name',)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'category',
+        'name',
+        'brand',
+        'weight',
+        'description',
+        'price',
+        'image',
+    )
+
+    ordering = ('category','brand','name',)
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
