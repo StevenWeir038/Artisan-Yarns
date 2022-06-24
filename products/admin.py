52627d8b740fb895a3d429b166403c37b4a3697e
StevenWeir038 <weir_steven@btinternet.com>
2022-06-24 18:25:18 +0000

    Update admin with search and filter

--- products/admin.py
+++ products/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Category, Product
 
 
+@admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = (
         'friendly_name',
@@ -10,19 +11,19 @@ class CategoryAdmin(admin.ModelAdmin):
 
     ordering = ('friendly_name',)
 
+
+@admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
+    search_fields = ['name', 'brand', 'description']
     list_display = (
-        'category',
         'name',
-        'brand',
+        'category',
         'weight',
+        'brand',
         'description',
         'price',
         'image',
     )
+    list_filter = ('category', 'weight', 'brand')
 
-    ordering = ('category','brand','name',)
-
-
-admin.site.register(Category, CategoryAdmin)
-admin.site.register(Product, ProductAdmin)
+    ordering = ('brand','name','price',)
