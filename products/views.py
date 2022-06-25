f377317d8bb360dfd4664746b9b97b1486ab0e99
StevenWeir038 <weir_steven@btinternet.com>
2022-06-25 13:29:24 +0000

    Check for valid input from searchbar GET request

--- products/views.py
+++ products/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
 from .models import Product
 
 # Pagination
@@ -6,12 +7,18 @@ from django.core.paginator import Paginator
 
 
 def all_products(request):
-    """ A view to show all products including sort by and search queries """
+    """ Show all products including sort by and search queries """
 
     # Set up pagination
     p = Paginator(Product.objects.all(), 10)
     page = request.GET.get('page')
     products = p.get_page(page)
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You dodn't enter any search criteria")
 
     context = {
         'products': products,
@@ -20,8 +27,8 @@ def all_products(request):
     return render(request, 'products/products.html', context)
 
 
-def product_detail(request, product_id):
-    """ A view to show individual product details """
+def prodsuct_detail(request, product_id):
+    """ how individual product details """
 
     product = get_object_or_404(Product, pk=product_id)
 
