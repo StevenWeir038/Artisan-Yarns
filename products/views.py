c9c5e55b7c6541db7f5b2052cb4b079a0ad83102
StevenWeir038 <weir_steven@btinternet.com>
2022-06-25 15:20:46 +0000

    Add search functionality by name and description

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
+from django.db.models import Q
 from .models import Product
 
 # Pagination
@@ -8,26 +9,34 @@ from django.core.paginator import Paginator
 
 def all_products(request):
     """ Show all products including sort by and search queries """
+    query = None
 
     # Set up pagination
-    p = Paginator(Product.objects.all(), 10)
+    products = Product.objects.all()
+    products_list = Paginator(Product.objects.all(), 10)
     page = request.GET.get('page')
-    products = p.get_page(page)
+    products_list = products_list.get_page(page)
 
     if request.GET:
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
-                messages.error(request, "You dodn't enter any search criteria")
+                messages.error(request, "You didn't enter any search criteria")
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
 
     context = {
         'products': products,
+        'products_list': products_list,
+        'search_term': query,
     }
 
     return render(request, 'products/products.html', context)
 
 
-def prodsuct_detail(request, product_id):
+def product_detail(request, product_id):
     """ how individual product details """
 
     product = get_object_or_404(Product, pk=product_id)
