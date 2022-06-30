432561aea8bb5bd5397c8fbda0cf6300fc4b6058
StevenWeir038 <weir_steven@btinternet.com>
2022-06-30 11:12:50 +0000

    Add context parameters to pass to request.session

--- basket/contexts.py
+++ basket/contexts.py
@@ -1,11 +1,13 @@
-
-
 def basket_contents(request):
 
     basket_items = []
     total = 0
     product_count = 0 
 
-    context {}
-    
+    context = {
+        'basket_items': basket_items,
+        'product_count': product_count,
+        'total': total,
+    }
+
     return context
