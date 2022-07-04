741c0087833c80e77d59c1cda4408ae41f3977ac
StevenWeir038 <weir_steven@btinternet.com>
2022-07-04 17:20:54 +0000

    Update checkout order model with postcode field

--- checkout/models.py
+++ checkout/models.py
@@ -9,14 +9,15 @@ from products.models import Product
 
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
+    date = models.DateTimeField(auto_now_add=True)
     full_name = models.CharField(max_length=50, null=False, blank=False)
-    email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
-    country = models.CharField(max_length=40, null=False, blank=False)
-    town_or_city = models.CharField(max_length=40, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
     street_address2 = models.CharField(max_length=80, null=True, blank=True)
-    date = models.DateTimeField(auto_now_add=True)
+    town_or_city = models.CharField(max_length=40, null=False, blank=False)
+    postcode = models.CharField(max_length=20, null=True, blank=True)
+    country = models.CharField(max_length=40, null=False, blank=False)
 
     # calc 3 fields using model method when order is saved.
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
