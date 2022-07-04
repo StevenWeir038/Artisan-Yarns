cbbf36dcb0abb78fd56249b2c9c67c13496c6d9f
StevenWeir038 <weir_steven@btinternet.com>
2022-07-04 17:35:33 +0000

    Update field order in orderadmin

--- checkout/admin.py
+++ checkout/admin.py
@@ -15,10 +15,9 @@ class OrderAdmin(admin.ModelAdmin):
                        'grand_total',)
 
     fields = ('order_number', 'date', 'full_name',
-              'email', 'phone_number', 'country',
-              'postcode', 'town_or_city', 'street_address1',
-              'street_address2', 'county', 'delivery_cost',
-              'order_total', 'grand_total',)
+              'phone_number', 'email', 'street_address1',
+              'street_address2', 'town_or_city', 'postcode', 'country',
+              'delivery_cost', 'order_total', 'grand_total',)
 
     list_display = ('order_number', 'date', 'full_name',
                     'order_total', 'delivery_cost',
