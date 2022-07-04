23f07bf5b573ee3fd95122777b30a7b386f8b58d
StevenWeir038 <weir_steven@btinternet.com>
2022-07-04 16:08:18 +0000

    Method to calc order inc delivery total

--- checkout/models.py
+++ checkout/models.py
@@ -29,6 +29,15 @@ class Order(models.Model):
         """
         return uuid.uuid4().hex.upper()
 
+    def update_total(self):
+        """
+        Update grand total each time a line item is added,
+        accounting for delivery costs.
+        """
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        self.grand_total = self.order_total + self.delivery_cost
+        self.save()
+
     def save(self, *args, **kwargs):
         """
         Override the original save method to set the order number
