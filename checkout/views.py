cee3000d94f3e7564517d168d27d3d8cbf6292da
StevenWeir038 <weir_steven@btinternet.com>
2022-07-04 19:22:00 +0000

    Update checkout view for basket content

--- checkout/views.py
+++ checkout/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 
 def checkout(request):
     """ View for checkout page """
+    basket = request.session.get('basket', {})
+    if not basket:
+        messages.error(request, "The basket is empty")
 
-
-    return render(request, 'checkout/checkout.html')
+    return redirect(reverse('products'))
