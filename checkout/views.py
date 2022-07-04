ab862350e276594c7822c8c6b7361ecd2e3439b2
StevenWeir038 <weir_steven@btinternet.com>
2022-07-04 20:10:36 +0000

    Pass orderform to template

--- checkout/views.py
+++ checkout/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 
+from .forms import OrderForm
+
 def checkout(request):
     """ View for checkout page """
     basket = request.session.get('basket', {})
@@ -8,3 +10,11 @@ def checkout(request):
         messages.error(request, "The basket is empty")
 
     return redirect(reverse('products'))
+
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
