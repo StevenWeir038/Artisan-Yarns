b5a85de77fb3abf16ec7d559659fa297383e9f5e
StevenWeir038 <weir_steven@btinternet.com>
2022-07-05 07:37:03 +0000

    Add checkout urls

--- checkout/views.py
+++ checkout/views.py
@@ -5,16 +5,18 @@ from .forms import OrderForm
 
 def checkout(request):
     """ View for checkout page """
-    basket = request.session.get('basket', {})
-    if not basket:
-        messages.error(request, "The basket is empty")
+    # basket = request.session.get('basket', {})
+    # if not basket:
+    #     messages.error(request, "The basket is empty")
 
-    return redirect(reverse('products'))
+    # return redirect(reverse('products'))
 
-    order_form = OrderForm()
-    template = 'checkout/checkout.html'
-    context = {
-        'order_form': order_form,
-    }
+    # order_form = OrderForm()
+    # template = 'checkout/checkout.html'
+    # context = {
+    #     'order_form': order_form,
+    # }
 
-    return render(request, template, context)
+    # return render(request, template, context)
+
+    return render(request, 'checkout/checkout.html')
