436148f97f54c9130e4c5dffa9b28f8b9da7973c
StevenWeir038 <weir_steven@btinternet.com>
2022-07-09 14:04:26 +0000

    Set up checkout view to return secrets

--- checkout/views.py
+++ checkout/views.py
@@ -1,22 +1,45 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
+from basket.contexts import basket_contents
+
+import stripe
 
 
 def checkout(request):
     """ View for checkout page """
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     basket = request.session.get('basket', {})
     if not basket:
         messages.error(request, "Your basket is empty!")
         return redirect(reverse('products'))
 
+    current_basket = basket_contents(request)
+    total = current_basket['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
+    print(intent)
+
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe publiv key is missing. \
+            Did you forget to set it in your environment?')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51L3xB3E1oNV7hjVnO5PIJiOhRd0s9gnna3eRGTH7AebvcHBaji5pE93HY9XHa3pcL8bDq9uFENunIXWKNpUGkc9d00ZENrzXnL',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
