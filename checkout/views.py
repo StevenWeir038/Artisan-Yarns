381e3f5e7bef54d56349d6b9501328846a0f1481
StevenWeir038 <weir_steven@btinternet.com>
2022-07-09 15:29:33 +0000

    Order model required for checkout success view

--- checkout/views.py
+++ checkout/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.conf import settings
 
-from .forms import OrderForm
+from .forms import Order, OrderForm
 from .models import OrderLineItem
 from products.models import Product
 
