00ef259d403f5ff4d1c245faa88a0b97c1a78147
StevenWeir038 <weir_steven@btinternet.com>
2022-07-11 08:42:16 +0000

    Install, setup, style countries for checkout form

--- checkout/models.py
+++ checkout/models.py
@@ -4,6 +4,8 @@ from django.db import models
 from django.db.models import Sum
 from django.conf import settings
 
+from django_countries.fields import CountryField
+
 from products.models import Product
 
 
@@ -18,7 +20,7 @@ class Order(models.Model):
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     county = models.CharField(max_length=40, null=True, blank=True)
     postcode = models.CharField(max_length=20, null=True, blank=True)
-    country = models.CharField(max_length=40, null=False, blank=False)
+    country = CountryField(blank_label="Country *", null=False, blank=False)
 
     # calc 3 fields using model method when order is saved.
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
