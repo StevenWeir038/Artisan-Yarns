3f3bf3b138cbfc56892e0e471c39af42d85a125a
StevenWeir038 <weir_steven@btinternet.com>
2022-07-12 18:14:44 +0000

    Post handler to save form and redirect to same page

--- profiles/views.py
+++ profiles/views.py
@@ -1,20 +1,28 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 
 from .models import UserProfile
 from .forms import UserProfileForm
 
 
 def profile(request):
-    """ User's profile page view """
-    profile = get_object_or_404(UserProfile, user=request.user)
+    """ Display the user's profile. """
+    my_profile = get_object_or_404(UserProfile, user=request.user)
 
-    form = UserProfileForm(instance=profile)
-    orders = profile.orders.all()
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=my_profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully')
+
+    form = UserProfileForm(instance=my_profile)
+    orders = my_profile.orders.all()
 
     template = 'profiles/profile.html'
     context = {
         'form': form,
         'orders': orders,
+        'on_profile_page': True
     }
 
     return render(request, template, context)
