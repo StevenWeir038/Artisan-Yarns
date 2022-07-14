430d90e6185ba6cbd0642f8a5de3c32292e65b4f
StevenWeir038 <weir_steven@btinternet.com>
2022-07-14 12:42:00 +0000

    Apply login dec to restrict function access

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def wishlist(request):
-    """ Wishlist page view """
+    """ 
+    Display a specific user's wishlst 
+    Need to to pass a dictionary similar to the basket to template
+    """
 
-    return render(request, 'wishlist/wishlist.html')
+    template = ''
+    context = {}
+
+    return render(request, 'wishlist/wishlist.html', context)
