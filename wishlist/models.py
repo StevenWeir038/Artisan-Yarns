e5095a85e784d0c909a8d38505654b561f8d69d4
StevenWeir038 <weir_steven@btinternet.com>
2022-07-14 12:22:15 +0000

    Code tidy

--- wishlist/models.py
+++ wishlist/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Wishlist(models.Model):
+    """ Wishlist model (for authenticated users only) """
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, default=1)
+
+    def __str__(self):
+        return f"({self.user_profile}'s) WishList"
