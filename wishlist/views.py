e3270d72105913bbb75067c06cab1cd493a1b2c8
StevenWeir038 <weir_steven@btinternet.com>
2022-07-14 14:56:54 +0000

    Pass wishlist object to template within context

--- wishlist/views.py
+++ wishlist/views.py
@@ -12,11 +12,11 @@ def wishlist(request):
     Need to to pass a dictionary similar to the basket to template
     """
     user = get_object_or_404(UserProfile, user=request.user)
-    wishlist = Wishlist.objects.filter(user_profile=user)
+    my_wishlist = Wishlist.objects.filter(user_profile=user)
 
     template = 'wishlist/wishlist.html'
     context = {
-        'wishlist': wishlist,
+        'my_wishlist': my_wishlist,
         }
 
     return render(request, template, context)
