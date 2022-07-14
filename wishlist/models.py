ae2b6e4c1097199f8d2e5c7af4395ff4e630d07c
StevenWeir038 <weir_steven@btinternet.com>
2022-07-14 12:24:44 +0000

    Forgot to add product/userprofile models

--- wishlist/models.py
+++ wishlist/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
+from profiles.models import UserProfile
+from products.models import Product
+
 
 class Wishlist(models.Model):
-    """ Wishlist model (for authenticated users only) """
+    """ Wishlist model (feature for authenticated users only) """
     user_profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE, default=1)
 
