47ab9ca9caf15227c066d4bb3d7d1438f03b4374
StevenWeir038 <weir_steven@btinternet.com>
2022-07-15 07:22:13 +0000

    Build delete_wishlist_item view

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.wishlist, name='wishlist'),
     path('add_wishlist_item/<int:product_id>/',
          views.add_wishlist_item, name='add_wishlist_item'),
+    path('delete_wishlist_item/<int:product_id>/',
+         views.delete_wishlist_item, name='delete_wishlist_item'),
 ]
