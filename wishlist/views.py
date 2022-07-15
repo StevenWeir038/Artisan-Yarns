1bf66cedbf1818c32cae52322f1bdc851df21b91
StevenWeir038 <weir_steven@btinternet.com>
2022-07-15 07:23:01 +0000

    Plan delete_wishlist_item view behaviours

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 from profiles.models import UserProfile
 from products.models import Product
@@ -8,10 +9,7 @@ from wishlist.models import Wishlist
 
 @login_required
 def wishlist(request):
-    """
-    Display a specific user's wishlst
-    Need to to pass a dictionary similar to the basket to template
-    """
+    """ Display a specific user's wishlst """
     user = get_object_or_404(UserProfile, user=request.user)
     my_wishlist = Wishlist.objects.filter(user_profile=user)
 
@@ -26,16 +24,25 @@ def wishlist(request):
 @login_required
 def add_wishlist_item(request, product_id):
     """
-    Add a wishlist item from a product
-    Think - get the product id from product details and pass to wishlist
+    Add a wishlist item from the current product on product_detail
     """
     user = get_object_or_404(UserProfile, user=request.user)
     product = get_object_or_404(Product, pk=product_id)
 
-    # Only need to create a wishlist object
-    # No need to render anything to a template
-    # Redirect back to same page as button
-    # This only exists in the product details template at the moment
     Wishlist.objects.create(user_profile=user, product=product)
 
     return redirect(reverse('product_detail', args=[product.id]))
+
+
+@login_required
+def delete_wishlist_item(request, product_id):
+    """
+    Add a wishlist item from the current product on product_detail
+    Same approach as add_wishlist_item - get user, get product_id
+    Link this view/function to a button/icon in wishlist.html template
+    Similar to Kennel project - find a single entry with a filter and delete
+    Feedback the action to the user
+    In this case just want to go back yo wishlist page to review remaining list
+    """
+    user = get_object_or_404(UserProfile, user=request.user)
+    product = get_object_or_404(Product, pk=product_id)
