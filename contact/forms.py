91c76eb23d457602a46668517424859a34670a38
StevenWeir038 <weir_steven@btinternet.com>
2022-07-15 18:41:32 +0000

    Add required for fields

--- contact/forms.py
+++ contact/forms.py
@@ -1,12 +1,9 @@
 from django import forms
 
 
-class ContactForm(forms.ModelForm):
-    """
-    Form to allow site users to communicate with site owners
-    Need get back to user we need a name, email, enquiry subject, message itself
-    """
-
-    class Meta:
-        
-
+class ContactForm(forms.Form):
+    """ Form to allow site users to communicate with site owners """
+    name = forms.CharField(min_length=2, max_length=100, label='Your name')
+    email = forms.EmailField(widget=forms.EmailInput(), label='Your Email')
+    subject = forms.CharField(min_length=3, max_length=40, label='Subject')
+    message = forms.CharField(min_length=5, max_length=300, widget=forms.Textarea(), label='Message')
