cd5717d365a8c38b34bb58967f1f764707d61f0a
StevenWeir038 <weir_steven@btinternet.com>
2022-07-16 18:06:42 +0000

    Add basic email model

--- newsletter/models.py
+++ newsletter/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+# A newsletter can be as simple as using one field.  In fact the less a user
+# has to do to subscribe, the more likely they are to do so.  Therefore, we
+# will only ask them to provide their email and click a submit button.
+
+class NewsletterSubscribe():
+    """
+    Newsletter subscription model
+    Also add a subscription time.  May be useful in future.
+    i.e. Removing old subs programatically after a certain duration.
+    Maybe have a new mail autosend each week for 10 weeks (use a counter).
+    So many options depending on the business (logic can be worked into view). 
+    """
+    email = models.EmailField()
+    subscribe_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.email
