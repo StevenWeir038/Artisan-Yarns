6ea0b7535c1a24ad116b827646e625bee58ffc31
StevenWeir038 <weir_steven@btinternet.com>
2022-07-16 04:51:32 +0000

    Comment view behaviours to code

--- contact/views.py
+++ contact/views.py
@@ -4,4 +4,22 @@ from django.shortcuts import render
 def contact(request):
     """ Contact page view """
 
+    # PLAN OF ATTACK
+
+    # 1st check the request method
+
+    # create a form instance
+
+    # clean the data
+
+    # if form is valid send the email
+    
+    # relay a success message to the user
+
+    # if for is not valid create relay a failure message to the user
+
+    # create a blank instance and allow user to repopulate
+
+    # apass the form to the template via the context dictionary
+
     return render(request, 'contact/contact.html')
