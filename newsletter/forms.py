e53bd6d9a4dea898596b34d6559668c381887a9d
StevenWeir038 <weir_steven@btinternet.com>
2022-07-17 09:33:50 +0000

    Build newsletter form

--- newsletter/forms.py
+++ newsletter/forms.py
@@ -1,1 +1,24 @@
 from django import forms
+from.models import NewsletterSub
+
+
+class NewsletterSubForm(forms.ModelForm):
+    """ Site newsletter form """
+    class Meta:
+        model = NewsletterSub
+        exclude = ('subscribe_date',)
+
+    def __init__(self, *args, **kwargs):
+        """
+            Add placeholders and classes, remove auto-generated
+            labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'email': 'Email Address',
+        }
+        for field in self.fields:
+            self.fields[field].widget.attrs[
+                'placeholder'] = "Enter your email address..."
+            self.fields[field].widget.attrs['class'] = 'form-input'
+            self.fields[field].label = False
