dfb4d388c9df22b7bb9b9b13e711026c9ae54124
StevenWeir038 <weir_steven@btinternet.com>
2022-07-17 14:37:36 +0000

    Prevent duplicate email entries on newsletter list

--- newsletter/contexts.py
+++ newsletter/contexts.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from .models import NewsletterSub
 from .forms import NewsletterSubForm
 
@@ -14,6 +15,11 @@ def subscription_form_contents(request):
         subscription_form = NewsletterSubForm(request.POST)
         # if data has no issues
         if subscription_form.is_valid():
+            # prevent a duplicate email being saved
+            instance = subscription_form.save(commit=False)  # don't save the instance
+            if Newsletter.objects.filter(email=email).exists():
+                # tell the user the email is already on the sub list so do nothing more before exiting loop
+
             # save the form to the variable
             subscription_form.save()
             # set the variable as the form instance
