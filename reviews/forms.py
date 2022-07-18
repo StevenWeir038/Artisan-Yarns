686485dd3dcfdc628ff3475cd0b2d2c7303d23bf
StevenWeir038 <weir_steven@btinternet.com>
2022-07-18 15:05:53 +0000

    Create form, include fields for user to fillout

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,1 +1,14 @@
 from django import forms
+from .models import ProductReview
+
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = ProductReview
+        fields = [
+            'rating', 'review_title', 'review_description',
+            ]
+        
+        review_description = forms.CharField(
+            widget=forms.Textarea()
+        )
