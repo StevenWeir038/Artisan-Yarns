9b8cce54fdc347f613d2cdeac79ebdc9a8c419d5
StevenWeir038 <weir_steven@btinternet.com>
2022-07-18 15:56:59 +0000

    Configure admin interface for easier queries

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import ProductReview
 
-# Register your models here.
+
+admin.site.register(ProductReview)
+class ProductReviewAdmin(admin.ModelAdmin):
+    """
+    Display list of Reviews
+    Allow admin to filter by ratings
+    Order by most recent reviews and ratings
+    """
+
+    model = ProductReview
+
+    search_fields = ['product', 'rating', 'user_profile']
+
+    list_display = (
+        'added_on',
+        'product',
+        'rating',
+        'review_description',
+        'user_profile',
+    )
+
+    list_filter = ('product', 'rating', 'user_profile',)
+
+    ordering = ('-added_on', '-rating', )
