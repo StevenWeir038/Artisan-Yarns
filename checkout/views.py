cedb7b7ef298d0bc7e5e0718182470b958e49e6f
StevenWeir038 <weir_steven@btinternet.com>
2022-07-20 15:34:43 +0000

    Forgot to import render_to_string

--- checkout/views.py
+++ checkout/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import (
 from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
+from django.template.loader import render_to_string
 
 import stripe
 import json
