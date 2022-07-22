dc1a630d7a27951e035da07d1285b39e6cce96da
StevenWeir038 <weir_steven@btinternet.com>
2022-07-22 04:39:32 +0000

    Fields not req to remove valid/invalid classes

--- contact/forms.py
+++ contact/forms.py
@@ -3,11 +3,11 @@ from django import forms
 
 class ContactForm(forms.Form):
     """ Form to allow site users to communicate with site owners """
-    name = forms.CharField(min_length=2, max_length=100, label='Your name')
-    email = forms.EmailField(widget=forms.EmailInput(), label='Your Email')
-    subject = forms.CharField(min_length=3, max_length=40, label='Subject')
+    name = forms.CharField(min_length=2, max_length=100, label='Your name', required=False)
+    email = forms.EmailField(widget=forms.EmailInput(), label='Your Email', required=False)
+    subject = forms.CharField(min_length=3, max_length=40, label='Subject', required=False)
     message = forms.CharField(
-        min_length=5, max_length=300, widget=forms.Textarea(), label='Message')
+        min_length=5, max_length=300, widget=forms.Textarea(), label='Message', required=False)
 
     def __init__(self, *args, **kwargs):
         """
