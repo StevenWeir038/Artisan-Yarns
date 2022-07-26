414a1ac5d7fa102d988cf2b459df6122e29756e3
StevenWeir038 <weir_steven@btinternet.com>
2022-07-26 19:56:55 +0000

    Build and migrate contact model

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100, null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
+    subject = models.CharField(max_length=40)
+    message = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
